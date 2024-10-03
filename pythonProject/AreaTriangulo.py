7765bff827d988792bef4a4b8e1a94a7f63935ca
OSCAR DAVID ANGULO VALENCIA <1058546220@fup.edu>
2024-10-03 15:01:25 -0500

    Se crea el proyecto con el archivo main y una funcion con el hola mundo

---
+++ pythonProject/AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Ejemplo para calcular el area del triangulo
+
+# Variables de entrada
+base = int(input("Ingrese la base: "))
+altura = int(input("Ingrese la altura: "))
+
+# Proceso
+def calcularAreaTriangulo(b,a):
+    
