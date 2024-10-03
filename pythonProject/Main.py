f875f2320064b11cb992ed9c6afb157d8d1a8b4a
OSCAR DAVID ANGULO VALENCIA <1058546220@fup.edu>
2024-10-03 14:52:13 -0500

    Se crea el proyecto con el archivo main y una funcion con el hola mundo

---
+++ pythonProject/Main.py
@@ -0,0 +1,12 @@
+# Definicion de una funcion
+def my_function():
+    print("Hello from a function")
+
+#Invocar la funcion
+my_function()
+
+def mostarNombre(nombre):
+    print("Mi nombre es"+nombre)
+
+nom = input("Ingrese su nombre: ")
+mostrarNombre(nom)
